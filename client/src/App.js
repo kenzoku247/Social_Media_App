c85b2a52e42382b53a70318ad87ca79058d3351d
kenzoku247 <vuphuc365@gmail.com>
2022-05-15 23:35:24 +0700

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import PageRender from './PageRender';
-import Register from './pages/register';
-import Login from './pages/login';
-import Home from './pages/home';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-      <input type="checkbox" name="" id="theme" />
-      <div className='App'>
-        <div className="main">
-          <Routes>
-            <Route path="/:page" exact element={<PageRender/>} />
-            <Route path="/:page/:id" exact element={<PageRender/>} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
     </Router>
   );
 }
--- models/README.md
+++
@@ -1,5 +0,0 @@
-# Model
-    Nhiệm vụ chính của thành phần này chỉ đơn giản là quản lý cấu trúc dữ liệu.
-
-## userModel
-    Cấu trúc dữ liệu của việc cấu thành nên 1 User
